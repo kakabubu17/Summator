c1e77173b2f46298812cd96a022336d5d55ae874
DaryaBurtovaya <msright@live.ru>
2015-04-23 20:25:38 +0600

    Add

---
+++ Server/Server/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace serverV2
+{
+    public class StateObject
+    {
+        // Client  socket
+        public Socket workSocket = null;
+        // Receive buffer
+        public byte[] buffer = new byte[1024];
+        // Received data string
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class AsynchronousSocketListener
+    {
+        public static int sum = 0;
+        // Thread signal
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public AsynchronousSocketListener()
+        {
+        }
+
+        public static void StartListening()
+        {
+            byte[] bytes = new Byte[1024];
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1);
+
+            // Create a TCP socket
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(20);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                    // Wait until a connection is made before continuing
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press ENTER to continue...");
+            Console.Read();
+
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue
+            allDone.Set();
+
+            // Get the socket that handles the client request
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, 1024, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = null;
+
+            // Retrieve the state object and the handler socket from the asynchronous state object
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                int a;
+                content = state.sb.ToString();
+                string[] split = content.Split(new Char[] { ' ', ',', '\t' });
+                for (int j = 0; j < split.Length; j++)
+                {
+                    if (Int32.TryParse(split[j], out a))
+                    {
+                        int[] num = split.Select(s => int.Parse(s)).ToArray();
+                        sum += num[j];
+                    }
+                }
+                Console.WriteLine("Sum = {0}", sum);
+            }
+        }
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+    }
+}
